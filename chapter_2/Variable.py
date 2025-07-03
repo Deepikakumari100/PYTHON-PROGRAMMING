9aba3924c0548a7219a85a3623742ee84f3d38e0
Boby Sharma <143808598+bobybhardwaj1@users.noreply.github.com>
2025-07-03 11:29:45 +0530

    Delete chapter_2 directory

--- chapter_2/Variable.py
+++
@@ -1,8 +0,0 @@
-a=2 #we have given the name of variable a 
-b=3
-c="Boby"
-d=print(a+b)
-# there are some rules to write the name of the variable 
-# It can start from an letter or underscore
-# it can contain digit , letter and underscore
-# it can't start with a digit 
--- chapter_2/input.py
+++
@@ -1,8 +0,0 @@
-a=input("enter your first number:")
-# it always takes input in string foramt 
-# if you want to take it in integer format or ant other format you have to define it
-b=int(input("enter your second number:"))
-print(type(a))
-print(type(b))
-c=float(input("enter your third number"))
-print(c)
