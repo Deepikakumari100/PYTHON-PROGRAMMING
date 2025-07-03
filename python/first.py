d603ea9838d1a0cf054b2a2ce5617678c019f68a
Boby Sharma <143808598+bobybhardwaj1@users.noreply.github.com>
2025-07-03 11:44:06 +0530

    Delete python directory

--- python/first.py
+++
@@ -1,4 +0,0 @@
-print("hello world")
-#this is a single line comment
-'''this is a multiline comment
-as you see'''
--- python/speakingcode.py
+++
@@ -1,4 +0,0 @@
-import pyttsx3
-engine = pyttsx3.init()
-engine.say("we are indians. we are the best")
-engine.runAndWait()
