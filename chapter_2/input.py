316a07123e0a4f5c62ba597b4cbf10c0a4ef51df
Boby Sharma <143808598+bobybhardwaj1@users.noreply.github.com>
2025-07-03 10:59:14 +0530

    Add files via upload

---
+++ chapter_2/input.py
@@ -0,0 +1,8 @@
+a=input("enter your first number:")
+# it always takes input in string foramt 
+# if you want to take it in integer format or ant other format you have to define it
+b=int(input("enter your second number:"))
+print(type(a))
+print(type(b))
+c=float(input("enter your third number"))
+print(c)
